a322dae543933e2d88eb1faf55726f3510bcb5db
maboglia <mauro.bogliaccino@gmail.com>
2022-02-23 14:50:10 +0100

    fine model

---
+++ alimenti/src/test/java/com/boglia/AlimentiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boglia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlimentiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
